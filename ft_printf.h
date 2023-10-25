e4b7cbb509b9694d8916c86739f6378fe2d92d71
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-26 01:01:12 +0300

    I wrote a function called ft_putchar of type void.
        It takes two parameters:
    
            1. const char c şeklinde -> bu yazdırmak istediğim karakter.
    
            2.  I initially defined a struct as follows:
                typedef struct s_printf for a specific purpose.
                However, I realized that I would need to use this structure in multiple places.
                As a result, I decided to define this struct in a file named ft_printf.h so that I could easily call it when needed.
                The struct is defined as follows:
    
            2.  typedef struct s_printf
                {
    	            va_list	arg_lst;
    	            int		len;
    	            char	format;
                }			t_printf;
    
                This approach allows me to use this struct conveniently throughout the project.
    
            if (!c)
                return;  The function ends when NULL is encountered.
            write(1, &c, 1); Write the received character to the standard output.
            p->len++; Increment the character count as characters are written.

--- ft_printf.h
+++ ft_printf.h
@@ -1,21 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/23 17:31:11 by agungor           #+#    #+#             */
-/*   Updated: 2023/10/26 00:56:05 by agungor          ###   ########.fr       */
+/*   Created: 2023/10/22 19:09:15 by agungor           #+#    #+#             */
+/*   Updated: 2023/10/22 19:09:17 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-void	ft_putchar(const char c, t_printf *p)
+# include <stdarg.h>
+# include <unistd.h>
+
+# define DEC "0123456789"
+# define HEXLOW "0123456789abcdef"
+# define HEXUP "0123456789ABCDEF"
+
+typedef struct s_printf
 {
-	if (!c)
-		return ;
-	write(1, &c, 1);
-	p->len++;
-}
+	va_list	arg_lst;
+	int		len;
+	char	format;
+}			t_printf;
+
+int			ft_printf(const char *s, ...);
+
+#endif
