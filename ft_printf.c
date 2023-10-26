6cf755ff4f49d526eaca921a043a34ffddfff2df
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-26 23:44:53 +0300

    MORE OPTIMIZED ---ENG---
    The commit message of the more optimized version of the previous commit is below.
    
    Optimized the previous commit even further.
    In the event that the 'printf' function fails to print and returns -1, I considered that the 'write' function also wouldn't be able to write.
    I incremented the 'length' variable within the existing 't_printf' structure, which represents the number of characters printed.
    If the printing operation is successful and returns 1, I return the increased 'length' value as the final result.
    However, in the case of an adverse situation, I terminate the program early with a return value of -1.
    
            DAHA OPTİMİZE ---TR---
    
    Bir önceki commitin daha optimize halinin commit mesajı aşağıda yer almaktadır.
    
    Önceki işlemi daha da optimize ettim.
    Eğer 'printf' işlemi başarısız olup -1 dönerse, 'write' işleminin de yazdıramayacağını göz önünde bulundurdum.
    Mevcut 't_printf' yapısında bulunan 'length' adlı değişkeni, yazdırılan karakter sayısını temsil etmek için artırdım.
    Eğer yazdırma işlemi başarılı olursa ve 1 dönerse, artırılmış 'length' değerini en son kullanarak return ediyorum.
    Ancak, olumsuz bir durumda programı -1 değeri ile erken sonlandırıyorum.
    
    return (prnt->lenght++, write(1, &c, 1));

--- ft_printf.c
+++ ft_printf.c
@@ -5,17 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/23 17:31:11 by agungor           #+#    #+#             */
-/*   Updated: 2023/10/26 00:56:05 by agungor          ###   ########.fr       */
+/*   Created: 2023/10/26 20:00:59 by agungor           #+#    #+#             */
+/*   Updated: 2023/10/26 23:39:01 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putchar(const char c, t_printf *p)
+static int	ft_putchar(char c, t_printf *prnt)
 {
-	if (!c)
-		return ;
-	write(1, &c, 1);
-	p->len++;
+	return (prnt->lenght++, write(1, &c, 1));
+}
+
+static int	ft_putstr(char *c, t_printf *prnt)
+{
+	if (!s)
+		s = "(null)";
+	while (*s)
+		if (ft_putchar(*s, prnt) == -1)
+			return (-1);
+	return (1);
+}
+
+static int	ft_format(t_printf *prnt)
+{
+	if (prnt->format == 'c')
+		return (ft_putchar(va_arg(prnt->args, int), prnt));
+	else if (prnt->format == 's')
+		return (ft_putchar(va_arg(prnt->args, char *), prnt));
+	else if (prnt->format == 'p')
+		return (ft_putchar(va_arg(prnt->args, )));
+}
+
+int	ft_printf(const char *s, ...)
+{
+	t_printf	prnt;
+
+	prnt.lenght = 0;
+	va_start(prnt.args, s);
+	while (*s)
+	{
+		if (*s == '%')
+		{
+			prnt.format = *++s;
+			if (ft_format(&ptr) == -1)
+				return (-1);
+		}
+		else if (ft_putchar(*s, &prnt) == -1)
+			return (-1);
+		s++;
+	}
 }
