7f763a936e8bf91c5a956524350ee059723ec222
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-27 01:10:18 +0300

    *** ENG COMMIT MESSSAGE ***
    About the ft_putstr function...
    
    "Firstly, I recognized that 's,' initially considered as a character array, is, in fact, a string.
    Realizing that I could accomplish this task with a simple 'while' loop using the 'ft_putchar' function, I proceeded.
    
    The original 'printf' function prints "(null)" when a NULL expression is provided.
    Therefore, I checked if the 's' pointer is NULL, and in such a case, I assigned "(null)" to the 's' pointer.
    
    The remaining operation is executed within the 'while' loop through the 'ft_putchar' function."
    
            *** TR COMMİT MESAJI ***
    
    ft_putstr fonksiyonu hakkında...
    
    Öncelikle, bir karakter dizisi olarak düşünülen 's' adlı bir string olduğunu belirledim.
    Ardından, basit bir 'while' döngüsü içinde 'ft_putchar' işlevini kullanarak bu görevi yerine getirebileceğimi fark ettim.
    
    Orijinal 'printf' işlevi, bir NULL ifadesi (null) girildiğinde "(null)" metnini yazdırır.
    Bu nedenle, 's' işaretçisinin NULL olduğunu kontrol ettim ve bu durumda 's' işaretçisine "(null)" olarak bir atama yaptım.
    
    Geri kalan işlem zaten 'while' döngüsü içinde 'ft_putchar' işlevi tarafından gerçekleştiriliyor.

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 20:00:59 by agungor           #+#    #+#             */
-/*   Updated: 2023/10/26 23:39:01 by agungor          ###   ########.fr       */
+/*   Updated: 2023/10/27 00:10:16 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,16 @@
 static int	ft_putchar(char c, t_printf *prnt)
 {
 	return (prnt->lenght++, write(1, &c, 1));
+}
+
+static int	ft_putstr(char *s, t_printf *prnt)
+{
+	if (!s)
+		s = "(null)";
+	while (*s)
+	{
+		if (ft_putchar(*s, prnt) == -1)
+			return (-1);
+		return (1);
+	}
 }
