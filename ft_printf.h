b3e4f9ff9868ef957277d91342b132cd2a2a3a3e
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-10-26 23:12:12 +0300

    ---ENG COMMIT MESSAGE---
        #ifndef FT_PRINTF_H
        # define FT_PRINTF_H
    // Defines the FT_PRINTF_H symbol if the above condition is not met.
    
        # include <stdarg.h>
    // Includes the required header file to be able to use variable argument functions like va_list, va_arg, va_start, va_end.
        # include <unistd.h>
    // Includes the required header file to be able to use the write function.
    
        # define DEC "0123456789"
    // Defines a constant containing decimal base digits.
        # define HEXLOW "0123456789abcdef"
    // Defines a constant containing hexadecimal digits in lowercase.
        # define HEXUP "0123456789ABCDEF"
    // Defines a constant containing hexadecimal digits in uppercase.
    
    typedef struct s_printf
    {
        int     length;
        // Represents the length to be used as the return value of the ft_printf function.
        char    format;
        // Controls any special character that comes after the "%" character.
        va_list args;
        // A va_list structure used to accept an unlimited number of parameters.
    }           t_printf;
    // A structure defined by yourself, named "t_printf."
    
    int         ft_printf(const char *s, ...);
    // Defines the prototype of the ft_printf function.
    
        #endif
    // Closes the FT_PRINTF_H symbol when the #ifndef condition is concluded.
    
            ---TR COMMIT MESSAGE---
    
        #ifndef FT_PRINTF_H
        # define FT_PRINTF_H
    // Yukarıdaki şartın sağlanmaması durumunda, FT_PRINTF_H sembolünü tanımlar.
    
        # include <stdarg.h>
    // va_list, va_arg, va_start, va_end gibi değişken argümanlı işlevleri kullanabilmek için gerekli başlık dosyasını dahil eder.
        # include <unistd.h>
    // write işlemini kullanabilmek için gerekli başlık dosyasını dahil eder.
    
        # define DEC "0123456789"
    // Onluk tabanlı rakamları içeren bir sabit tanımlar.
        # define HEXLOW "0123456789abcdef"
    // Altıgen tabanlı rakamları içeren bir sabit tanımlar (küçük harfle).
        # define HEXUP "0123456789ABCDEF"
    // Altıgen tabanlı rakamları içeren bir sabit tanımlar (büyük harfle).
    
    typedef struct s_printf
    {
        int     length;
        // ft_printf fonksiyonunun dönüş değeri olarak kullanılacak uzunluğu temsil eder.
        char    format;
        // "%" karakterinden sonra gelen herhangi bir özel karakterin kontrolünü sağlar.
        va_list args;
        // Sonsuz sayıda parametre alabilen bir va_list yapısıdır.
    }           t_printf;
    // Kendi tanımladığınız bir yapıdır, "t_printf" adını verirsiniz.
    
    int         ft_printf(const char *s, ...);
    // ft_printf fonksiyonunun prototipini tanımlar.
    
        #endif
    // #ifndef koşulu sona erdiğinde, FT_PRINTF_H sembolünü kapatır.

--- ft_printf.h
+++ ft_printf.h
@@ -5,8 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/26 03:01:21 by agungor           #+#    #+#             */
-/*   Updated: 2023/10/26 03:01:23 by agungor          ###   ########.fr       */
+/*   Created: 2023/10/26 19:56:07 by agungor           #+#    #+#             */
+/*   Updated: 2023/10/26 22:41:37 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+# define DEC "0123456789"
+# define HEXLOW "0123456789abcdef"
+# define HEXUP "0123456789ABCDEF"
+
+typedef struct s_printf
+{
+	int		lenght;
+	char	format;
+	va_list	args;
+}			t_printf;
+
+int			ft_printf(const char *s, ...);
+
+#endif
